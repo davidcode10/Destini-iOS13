135996b6dda7c6ae387a1176a1048e2ada3fc2e5
davidcode10 <davidedson2030@outlook.com>
2022-05-26 21:49:45 +0200

    code updated

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,13 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    
-    let example = [
-        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice1Destination: 1, choice2: "Take a right.", choice2Destination: 1),
-        Story(title: "You fall", choice1: "You get up", choice1Destination: 2, choice2: "You sleep", choice2Destination: 2),
-        Story(title: "You wake up", choice1: "You start to run", choice1Destination: 0, choice2: "You are dead", choice2Destination: 0)
-    ]
-    
+
     var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
